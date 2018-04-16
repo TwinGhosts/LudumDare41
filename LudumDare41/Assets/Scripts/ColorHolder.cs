74c9f74af42f42396bddb86a87b095c5f61d7c0c
Joeri Vervaat <joerivervaat94@hotmail.com>
2018-04-17 00:06:31 +0200

    - Added Color class for easy referencing and added some other base classes for utility

---
+++ LudumDare41/Assets/Scripts/ColorHolder.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorHolder : MonoBehaviour
+{
+    [Header("Normal")]
+    public Color red;
+    public Color orange;
+    public Color yellow;
+    public Color yellowGreen;
+    public Color green;
+    public Color mint;
+    public Color cyan;
+    public Color blue;
+    public Color purple;
+    public Color pink;
+
+    [Header("Dark")]
+    public Color darkRed;
+    public Color darkOrange;
+    public Color darkYellow;
+    public Color darkYellowGreen;
+    public Color darkGreen;
+    public Color darkMint;
+    public Color darkCyan;
+    public Color darkBlue;
+    public Color darkPurple;
+    public Color darkPink;
+
+    [Header("Light")]
+    public Color lightRed;
+    public Color lightOrange;
+    public Color lightYellow;
+    public Color lightYellowGreen;
+    public Color lightGreen;
+    public Color lightMint;
+    public Color lightCyan;
+    public Color lightBlue;
+    public Color lightPurple;
+    public Color lightPink;
+
+    [Header("Grayscale")]
+    public Color white;
+    public Color lightGray;
+    public Color gray;
+    public Color darkGray;
+    public Color black;
+}
